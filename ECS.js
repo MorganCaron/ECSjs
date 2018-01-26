5555db6231be9cf5dc40c2aa61013d5468508d61
Morgan Caron <caron.morgan91@gmail.com>
2018-01-27 00:16:04 +0100

    stable project

--- ECS.js
+++ ECS.js
@@ -1,40 +1,110 @@
-nbEntities = 0;
-
-class Component {
-    constructor(name) {
-        this.componentName = name;
-    }
+function lowercaseFirstLetter(string) {
+    return string.charAt(0).toLowerCase() + string.slice(1);
 }
 
 class Entity {
     constructor(name) {
-        this.entityId = (+new Date()).toString(16) + (Math.random() * 100000000).toString(16) + nbEntities;
+        this.entityId = nbEntities++;
         this.entityName = name;
-        this.components = {};
-        nbEntities++;
+        this.tags = [];
     }
 
     addComponent(component) {
-        this.components[component.componentName] = component;
+        this[lowercaseFirstLetter(component.name)] = new component;
+        return this;
+    }
+    removeComponent(component) {
+        var index = this.indexOf(tag);
+        if (this[lowercaseFirstLetter(component.name)] != undefined)
+            delete this[lowercaseFirstLetter(component.name)];
+        return this;
+    }
+    hasComponent(component) {
+        return (this[lowercaseFirstLetter(component.name)] != undefined);
+    }
+    hasComponents(components) {
+        for (var key in components) {
+            if (!hasComponent(components[key]))
+                return false;
+        }
+        return true;
+    }
+
+    addTag(tag) {
+        this.tags.push(tag);
+        return this;
+    }
+    removeTag(tag) {
+        var index = this.tags.indexOf(tag);
+        if (index != -1)
+            this.tags[index].remove();
+        return this;
+    }
+    hasTag(tag) {
+        return (this.tags.indexOf(tag) != -1);
+    }
+    hasTags(tags) {
+        for (var key in tags) {
+            if (!hasTag(tags[key]))
+                return false;
+        }
+        return true;
+    }
+}
+var nbEntities = 0;
+
+class EntityManager {
+    constructor() {
+        this.entities = [];
+    }
+
+    createEntity() {
+        var entity = new Entity();
+        this.entities.push(entity);
+        return entity;
+    }
+
+    all() {
+        return this.entities;
+    }
+
+    queryComponent(component) {
+        var result = [];
+        this.entities.forEach(function(entity) {
+            if (entity.hasComponent(component))
+                result.push(entity);
+        });
+        return result;
+    }
+
+    queryTag(tag) {
+        var result = [];
+        this.entities.forEach(function(entity) {
+            if (entity.hasTag(tag))
+                result.push(entity);
+        });
+        return result;
     }
 }
 
 class ECS {
     constructor() {
-        this.entities = {};
-        this.components = {};
+        this.entities = new EntityManager();
+        this.systems = [];
     }
 
-    addEntity(entity) {
-        this.entities[entity.entityId] = entity;
+    addSystem(system) {
+        this.systems.push(new system);
     }
-
-    addComponent(component) {
-        this.components[component.componentName] = component;
+    addSystems(systems) {
+        for (var key in systems)
+            this.systems.push(new systems[key]);
     }
 
     update() {
-        for (var key in this.components)
-            this.components[key].update(this.entities);
+        var entities = this.entities;
+        this.systems.forEach(function(system) {
+            system.update(entities);
+        });
     }
 }
--- test/script.js
+++ test/script.js
@@ -2,69 +2,104 @@ function degtorad(degres) {
     return (degres * (2 * Math.PI) / 360);
 }
 
-class CircleEntity extends Entity {
-    constructor(x, y, radius, color) {
-        super("circle");
+class CircleComponent {
+    constructor() {
+        this.x = 0;
+        this.y = 0;
+        this.radius = 0;
+        this.color = 'black';
+    }
+
+    init(x, y, radius, color) {
         this.x = x;
         this.y = y;
         this.radius = radius;
         this.color = color;
     }
-
-    draw() {
-        context.beginPath();
-        context.moveTo(this.x, this.y - this.radius);
-        for (var i = 0; i <= 360; i += 10)
-            context.lineTo(this.x + Math.cos(degtorad(i)) * this.radius, this.y - Math.sin(degtorad(i)) * this.radius);
-        context.closePath();
-        context.fillStyle = this.color;
-        context.lineCap = "round";
-        context.fill();
-    }
 }
 
-class CircleAppearanceComponent extends Component {
+class CanvasComponent {
     constructor() {
-        super("circleAppearance");
+        this.element = null;
+        this.context = null;
+        this.mouseX = 0;
+        this.mouseY = 0;
     }
 
-    update(entities) {
-        for (var key in entities)
-            entities[key].draw();
+    init(id) {
+        this.element = document.getElementById(id);
+        this.context = this.element.getContext('2d');
+    }
+
+    updateMousePos(element, evt) {
+        var rect = element.getBoundingClientRect();
+        this.mouseX = evt.clientX - rect.left,
+        this.mouseY = evt.clientY - rect.top
     }
 }
 
-class GravityComponent extends Component {
+class CanvasSystem {
+    update(entities) {
+        var canvasEntities = entities.queryComponent(CanvasComponent);
+        canvasEntities.forEach(function(canvas) {
+            canvas.canvasComponent.context.clearRect(0, 0, 600, 400);
+        });
+    }
+}
+
+class CircleSystem {
+    update(entities) {
+        var canvasEntities = entities.queryComponent(CanvasComponent);
+        var circleEntities = entities.queryComponent(CircleComponent);
+        canvasEntities.forEach(function(canvas) {
+            circleEntities.forEach(function(circle) {
+                circle.circleComponent.x = canvas.canvasComponent.mouseX;
+                circle.circleComponent.y = canvas.canvasComponent.mouseY;
+                canvas.canvasComponent.context.beginPath();
+                canvas.canvasComponent.context.moveTo(circle.circleComponent.x, circle.circleComponent.y - circle.circleComponent.radius);
+                for (var i = 0; i <= 360; i += 10)
+                    canvas.canvasComponent.context.lineTo(circle.circleComponent.x + Math.cos(degtorad(i)) * circle.circleComponent.radius, circle.circleComponent.y - Math.sin(degtorad(i)) * circle.circleComponent.radius);
+                canvas.canvasComponent.context.closePath();
+                canvas.canvasComponent.context.fillStyle = circle.circleComponent.color;
+                canvas.canvasComponent.context.lineCap = 'round';
+                canvas.canvasComponent.context.fill();
+            });
+        });
+    }
+}
+
+class App {
     constructor() {
-        super("gravity");
+        this.ecs = new ECS();
+
+        this.ecs.addSystems([
+            CanvasSystem,
+            CircleSystem
+        ]);
+
+        var canvas = this.ecs.entities.createEntity();
+        canvas.addComponent(CanvasComponent);
+        canvas.canvasComponent.init('canvas');
+        canvas.canvasComponent.element.addEventListener('mousemove', function(evt) {
+            canvas.canvasComponent.updateMousePos(this, evt);
+        }, false);
+
+        var circle = this.ecs.entities.createEntity();
+        circle.addComponent(CircleComponent);
+        circle.circleComponent.init(0, 0, 80, 'red');
     }
 
-    update(entities) {
-        for (var key in entities)
-            entities[key].y += 1;
+    update() {
+        this.ecs.update();
     }
 }
 
-var ecs, canvas, context;
-function main() {
-    canvas = document.getElementById("canvas");
-    context = canvas.getContext("2d");
-
-    ecs = new ECS();
-    var circleAppearanceComponent = new CircleAppearanceComponent();
-    ecs.addComponent(circleAppearanceComponent);
-    var gravityComponent = new GravityComponent();
-    ecs.addComponent(gravityComponent);
-    var circleEntity = new CircleEntity(100, 100, 80, "red");
-    circleEntity.addComponent(gravityComponent);
-    ecs.addEntity(circleEntity);
-    requestAnimationFrame(loop);
-}
-
-function loop() {
-    context.clearRect(0, 0, 600, 400);
-    ecs.update();
-    requestAnimationFrame(loop);
-}
-
-document.addEventListener('DOMContentLoaded', main, false);
+var app;
+document.addEventListener('DOMContentLoaded', function() {
+    app = new App();
+    requestAnimationFrame(update);
+}, false);
+function update() {
+    app.update();
+    requestAnimationFrame(update);
+};
